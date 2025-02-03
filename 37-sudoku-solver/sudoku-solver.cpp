dba92e45381d207be0fba39133a3079141029f53
vaidyashubham7578 <vaidyashubham7578@gmail.com>
2025-02-03 23:59:24 +0530

    Time: 232 ms (79.57%) | Memory: 8.7 MB (63.55%) - LeetSync

---
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    bool isSafe(vector<vector<char>>& board, int row, int col, int dig){
+        //Horinzontal
+        for(int i=0; i<9; i++){
+            if(board[row][i]==dig){
+                return false;
+            }
+        }
+
+        // Vertical
+        for(int j=0; j<9; j++){
+            if(board[j][col]==dig){
+                return false;
+            }
+        }
+
+        // Grid
+        int srow=(row/3)*3;
+        int scol=(col/3)*3;
+        for(int i=srow; i<=srow+2; i++){
+            for(int j=scol; j<=scol+2; j++){
+                if(board[i][j]==dig){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    bool helper(vector<vector<char>>& board, int row, int col){
+        if(row==9){
+            return true;
+        }
+
+        int nextRow=row, nextCol=col+1;
+        if(nextCol==9){
+            nextRow=row+1;
+            nextCol=0;
+        }
+        if(board[row][col]!='.'){
+            return helper(board, nextRow, nextCol);
+        }
+
+        // place the digit
+        for(char dig='1'; dig<='9'; dig++){
+            if(isSafe(board, row, col, dig)){
+                board[row][col]=dig;
+                if(helper(board, nextRow, nextCol)){
+                    return true;
+                }
+
+                board[row][col]='.'; // backtrackng
+            }
+            
+            
+        }
+        return false;
+    }
+
+    void solveSudoku(vector<vector<char>>& board) {
+        helper(board, 0, 0);
+
+    }
+};
