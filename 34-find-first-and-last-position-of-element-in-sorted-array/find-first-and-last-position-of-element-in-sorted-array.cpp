783d93050315ad0ac968c96761ceedd113e7c73a
vaidyashubham7578 <vaidyashubham7578@gmail.com>
2026-03-30 16:37:26 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (19.10%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,36 +1,42 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int tar) {
+    int findfirst(vector<int>& nums, int tar){
         int n=nums.size();
-        int first=-1;
-        int last=-1;
-        // int st=0, end=n-1;
-        // while(st<=end){
-        //     int mid=st+(end-st)/2;
-        //     if(nums[st]==tar){
-
-        //     }else if(nums[st]<tar){
-        //         end=mid-1;
-        //     }else{
-        //         st=mid+1;
-        //     }
-        // }
-
-        for(int i=0; i<n; i++){
-            if(nums[i]==tar){
-                first=i;
-                break;
+        int st=0, end=n-1;
+        int ans=-1;
+        while(st<=end){
+            int mid=st+(end-st)/2;
+            if(nums[mid]==tar){
+                ans=mid;
+                end=mid-1;
+            }else if(nums[mid]<tar){
+                st=mid+1;
+            }else{
+                end=mid-1;
             }
         }
+        return ans;
+    }
 
-        for(int i=n-1; i>=0; i--){
-            if(nums[i]==tar){
-                last=i;
-                break;
+    int findlast(vector<int>&nums, int tar){
+        int n=nums.size();
+        int st=0, end=n-1;
+        int ans=-1;
+        while(st<=end){
+            int mid=st+(end-st)/2;
+            if(nums[mid]==tar){
+                ans=mid;
+                st=mid+1;
+            }else if(nums[mid]<tar){
+                st=mid+1;
+            }else{
+                end=mid-1;
             }
         }
-
-        return {first, last};
+        return ans;
+    }
+    vector<int> searchRange(vector<int>& nums, int tar) {
+        return {findfirst(nums, tar), findlast(nums, tar)};
         
     }
 };
